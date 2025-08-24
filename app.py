c57a0c7ab30f62dc0a7f6aa6d228be8cdae60af7
jackxcreation <jodjack64@gmail.com>
2025-08-24 19:45:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -293,4 +293,5 @@ def update_cookies(platform):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
